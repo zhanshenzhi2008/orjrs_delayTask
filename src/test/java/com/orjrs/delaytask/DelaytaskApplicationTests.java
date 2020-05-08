f05876c9e232b434b2959fd8b0ba84419b1e9a77
zhanshenzhi2008 <lj2010vip@163.com>
2020-05-08 16:57:11 +0800

    延时任务

---
+++ src/test/java/com/orjrs/delaytask/DelaytaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orjrs.delaytask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelaytaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
